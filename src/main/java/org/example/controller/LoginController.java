c8127e429ebf99fb1c79e8bcaca132d601ea85ef
Sonia <soniachoo.zhu@gmail.com>
2020-07-19 17:47:21 +0800

    add admin role based access control

--- src/main/java/org/example/controller/LoginController.java
+++ src/main/java/org/example/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,10 +15,11 @@ import java.util.Map;
 public class LoginController {
 
     @GetMapping("/name")
-    public Map showName(){
+    public Map showName(HttpSession session){
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
         Map map=new HashMap();
         map.put("name",name);
+        session.setAttribute("operationUser",name);
         return map;
     }
 
